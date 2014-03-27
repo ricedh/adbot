30b0b17d922037eddff92795dac5ce34e2c7157a
Caleb McDaniel <caleb.mcdaniel@rice.edu>
2014-03-27 15:34:53 -0500

    adding pseudo code

--- adbot.py
+++ adbot.py
@@ -11,10 +11,19 @@ access_token_secret = settings['access_token_secret']
 # Code to compose tweet from runaway ads will go here
 # print tweet at end for local testing
 
-tweet = 'Stay tuned for more!'
+    # Get one of the ad files from an input "new" directory.
+	# Move file to "old" directory so next pass won't tweet it.
+
+    # Excerpt the largest number of words from the beginning of the
+    # ad that ads up to no more than 117 characters (including spaces
+	# and an ellipsis and quotation marks to indicate that it's an excerpt)
+    # Links will be shortened by Twitter API to 22 characters
+
+	# Recompose permalink to PoTH from ad filename, setting zoom to 5
+
+	# Put together the excerpt and the permalink and assign to `tweet` var
+
 print tweet
-
-
 
 # Try to post tweet variable to Twitter
 # Commented out for local testing without posting
